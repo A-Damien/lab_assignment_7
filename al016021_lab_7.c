b94f386918d582f77c777c3357858a1e872304e3
ADamien <ajd2024@gmail.com>
2023-03-22 13:09:28 -0400

    Added files for lab 7.

--- al016021_lab_7.c
+++ al016021_lab_7.c
@@ -8,18 +8,19 @@ void swap(int *xp, int *yp){
     *yp = temp;
 }
 
-void bubbleSort(int* array, int n, int* pass){
-    
+void bubbleSort(int* array, int n){
+    int pass = 0;
     for(int i = 0; i < n - 1; i++){
         int swaps = 0;//resets the number of swaps after each pass
         for(int j = 0; j < n - i - 1; j++){
             if(array[j] > array[j + 1]){
                 swap(&array[j], &array[j + 1]); 
                 swaps++;//increment number of swaps after each swap occurs
+                
             }
         }
-        (*pass)++;//updates the pointer keeping track of the number of passes
-        printf("Pass %d: %d\n", *pass, swaps);//prints out the current pass and the number of swaps needed
+        pass++;//updates the pointer keeping track of the number of passes
+        printf("Pass %d: %d\n", pass, swaps);//prints out the current pass and the number of swaps needed
     }
 }
 
@@ -35,12 +36,9 @@ int main(){
 
     int array[] = {97, 16, 45, 63, 13, 22, 7, 58, 72};
     int size = (sizeof(array) / sizeof(array[0]));
-    int pass = 0;
-    int* passPtr;//int pointer to keep track of the number of passes
-    passPtr = &pass;
     printf("Unsorted Array: ");
     print(array, size);
-    bubbleSort(array, size, passPtr);
+    bubbleSort(array, size);
     printf("Sorted Array: ");
     print(array, size);
 
